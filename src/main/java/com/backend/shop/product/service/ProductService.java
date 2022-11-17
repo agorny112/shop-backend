5bd1f54d359b49c670d96782af7aac42e02d2fda
agorn <agorny112@gmail.com>
2022-11-17 03:49:15 +0100

    adding Page and Pageable to response

--- src/main/java/com/backend/shop/product/service/ProductService.java
+++ src/main/java/com/backend/shop/product/service/ProductService.java
@@ -3,9 +3,9 @@ package com.backend.shop.product.service;
 import com.backend.shop.product.model.Product;
 import com.backend.shop.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +13,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
     }
 }
